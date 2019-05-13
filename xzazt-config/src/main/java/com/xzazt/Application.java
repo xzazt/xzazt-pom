56982d5e9c50f6a6d339b9d3cb781e79b5b9fdff
xz <xz>
2019-05-13 22:20:45 +0800

    代码提交

---
+++ xzazt-config/src/main/java/com/xzazt/Application.java
@@ -0,0 +1,12 @@
+package com.xzazt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        System.out.println("启动结束");
+    }
+}
